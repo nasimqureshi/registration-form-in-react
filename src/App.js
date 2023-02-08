8e59dac2cb3b1004932505925538a153c2b3b5b8
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-07 21:59:23 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Register from "./Components/Register";
+import Greet from "./Components/Greet";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isRegister: false,
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.isRegister ? <Greet></Greet> : <Register></Register>}
+      </div>
+    )
+  }
+}
+
+export default App
+
