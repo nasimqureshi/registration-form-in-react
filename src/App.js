dc026c530ef0fcdd893b39ccabf004d5a407ce52
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-07 22:11:19 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
     return (
       <div>
         {this.state.isRegister ? <Greet></Greet> : <Register></Register>}
+        <Greet />
+        <Register />
       </div>
     )
   }
---
+++ src/Components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Greet() {
+  return (
+    <div>
+      <form>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input type='text' name='name' />
+        </div>
+      </form>
+    </div>
+  )
+}
