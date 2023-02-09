bea536fc8122a456c2f407eb53eecf40d5a7f3f5
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-08 20:20:44 -0600

    commit

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -3,11 +3,21 @@ import React from 'react'
 export default function Register() {
   return (
     <div className='container card p-3 mt-2 register-container'>
+    <h1>Registration Form </h1>
       <form>
         <div className='form-group'>
-            <label htmlFor='name'>Name</label>
+            <label htmlFor='name'>Name:</label>
             <input type='text' name='name' className='form-control' />
         </div>
+        <div className='form-group'>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' name='email' className='form-control' />
+        </div>
+        <div className='form-group'>
+            <label htmlFor='pasword'>Password:</label>
+            <input type='password' name='password' className='form-control' />
+        </div>
+        <button type='submit' className='btn btn-primary'>Register</button>
       </form>
     </div>
   )
